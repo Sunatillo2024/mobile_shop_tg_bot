8c01a97f3f1241cd59b84fe297823249eec1581e
Sunatillo2024 <171573809+Sunatillo2024@users.noreply.github.com>
2025-09-27 13:42:46 +0500

    optimization

--- app/database/db.py
+++ app/database/db.py
@@ -1,9 +1,13 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from app.utils.logger import the_logger
+from dotenv import load_dotenv
 
+load_dotenv()
 # Подключение к базе данных
-DATABASE_URL = "postgresql://postgres:1299@localhost/Tg_bot_db_shop"
+DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
