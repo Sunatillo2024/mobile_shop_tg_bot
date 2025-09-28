7b78154f1a56fdde396e31a2cba7be2071be487f
Sunatillo2024 <171573809+Sunatillo2024@users.noreply.github.com>
2025-09-28 10:40:30 +0500

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	app/database/models.py

--- app/utils/Memory_abc.py
+++
@@ -1,6 +0,0 @@
-from aiogram.fsm.state import State
-from aiogram.fsm.context import FSMContext
-
-
-class My_address_FSMContext(State):
-    name_location = State()
--- app/utils/logger.py
+++
@@ -1,9 +0,0 @@
-# logger.py
-import logging
-import sys
-
-# Configure logging to output messages to stdout
-logging.basicConfig(level=logging.INFO, stream=sys.stdout)
-
-# Create a logger instance to use in other files
-the_logger = logging.getLogger(__name__)
